3e7820b502d0662329abc17f76ecf1dd051b019f
baraatarig091-dev <baraatarig091@gmail.com>
2026-07-01 10:22:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("baraa")
-print("software engineering")
-print("first year")
+student_name = "baraa"
+student_age = 20
+student_gba = 6.7
+is_enrolled =True
 
+print("name is ",student_name,"\n", "age is", student_age,"\n","gba is",student_gba,"\n","enrolled is?",is_enrolled)
