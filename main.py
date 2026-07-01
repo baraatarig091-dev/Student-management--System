dccb32ef7ee95b9a58e601e2b2ba8c3a076755a7
baraatarig091-dev <baraatarig091@gmail.com>
2026-07-01 13:57:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ student_age = 20
 student_gba = 6.7
 is_enrolled =True
 
-print("name is ",student_name,"\n", "age is", student_age,"\n","gba is",student_gba,"\n","enrolled is?",is_enrolled)
+print("name is: ",student_name,"\n", "age is:", student_age,"\n","gba is:",student_gba,"\n","enrolled is?",is_enrolled)
